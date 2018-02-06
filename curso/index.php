f52087754b93cffccc3a822ff5d734ac980f4411
Frontend Update <arte@update.pe>
2018-02-06 12:32:44 -0500

    actualizacion

--- curso/index.php
+++ curso/index.php
@@ -41,7 +41,7 @@
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
-  <title>CashTraidingPeru</title>
+  <title>Seminario Internacional de Economía Colaborativa</title>
 
   <!-- CSS  -->
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
@@ -82,7 +82,7 @@
 <ul id="slide-out" class="side-nav">
     <li>
         <div class="user-view">
-        <img  src="img/logo.svg" style="width: 50%; height: 50%;" >
+        <img  src="img/logo.svg" style="width: 50%; height: 50%;" class="responsive-img" >
         <span class="black-text name">Podemos Informarte</span>
         <span class="black-text email">Solo llenas tus Datos</span>
         </div>
@@ -130,15 +130,15 @@
         <a class="brand-logo que"> <a href="#" data-activates="slide-out" class="button-collapse que">Podemos Informarte</a></a>
           <div class="row hide-on-med-and-down">
             <div class="col s2 input-field">
-              <img src="img/logo.svg" style="width: 70%; height: 70%; padding-top: 10px;">
+              <img src="img/logo.png" style="width: 70%; height: 70%; padding-top: 10px;">
             </div>
     <form action="index.php" method="POST">
             <div class="col s2 input-field"> 
-              <label for="first_name" class="white-text">Nombres Apellido</label>
+              <label for="first_name" class="white-text thin">Nombres Apellido</label>
               <input placeholder="" id="nombre" type="text" name="nombre" required>
             </div>
               <div class="col s2 input-field">  
-                <label for="first_name" class="white-text" style="margin-top: -30px;">Quieres saber</label>
+                <label for="first_name" class="white-text thin" style="margin-top: -34px; font-size: 12px;">Quieres saber</label>
                   <select name="asunto" id="asunto" required>
                     <option value="" disabled selected></option>
                     <option value="Escuela de traiding" class="black-text">Escuela de traiding</option>
@@ -147,11 +147,11 @@
                   </select>
              </div>
              <div class="col s2 input-field">  
-              <label for="first_name" class="white-text">Correo</label>
+              <label for="first_name" class="white-text thin">Correo</label>
               <input placeholder="" id="correo" type="text" name="correo" required>
             </div>
             <div class="col s2 input-field">  
-              <label for="first_name" class="white-text">Telefono</label>
+              <label for="first_name" class="white-text thin">Telefono</label>
               <input placeholder="" id="telefono" type="text" name="telefono" required>
             </div>
             <div class="col s2 input-field"> 
@@ -168,7 +168,12 @@
 <!--navbarform-->
 
 
-
+<style type="text/css">
+  
+  .textleft{
+    margin: 0px 0px 0px;
+  }
+</style>
 
 
 <!--slider-->
@@ -176,48 +181,44 @@
     <ul class="slides">
       <li>
         <img src="img/slide1.jpg"> <!-- random image -->
+        <div class="caption right-align textleft">
+          <h3 class="thin" style="color: #8e24aa;">APRENDE A CONTROLAR Y A HACER CRECER TU DINERO</h3>
+          <h5 class="light text-lighten-3" style="color: #8e24aa;">Aprende todo acerca de la nueva economía Colaborativa y digital</h5>
+        </div>
+      </li>
+ <!--       <li>
+        <img src="img/slide1.jpg"> 
         <div class="caption right-align">
           <h3 class="thin" style="color: #8e24aa;">This is our big Tagline!</h3>
           <h5 class="light text-lighten-3" style="color: #8e24aa;">Here's our small slogan.</h5>
         </div>
       </li>
        <li>
-        <img src="img/slide1.jpg"> <!-- random image -->
+        <img src="img/slide1.jpg"> 
         <div class="caption right-align">
           <h3 class="thin" style="color: #8e24aa;">This is our big Tagline!</h3>
           <h5 class="light text-lighten-3" style="color: #8e24aa;">Here's our small slogan.</h5>
         </div>
-      </li>
-       <li>
-        <img src="img/slide1.jpg"> <!-- random image -->
-        <div class="caption right-align">
-          <h3 class="thin" style="color: #8e24aa;">This is our big Tagline!</h3>
-          <h5 class="light text-lighten-3" style="color: #8e24aa;">Here's our small slogan.</h5>
-        </div>
-      </li>
+      </li> -->
     </ul>
   </div> 
 <!--slider-->
 
-
 <div class="row" style="background-color: #2f1448;" align="center">
-  <div class="col s12 white-text" style="padding-bottom: 12%; padding-top: 12%;">
-    <h3 class="thin">Aqui va un titulo</h3>
+  <div class="col s12 white-text" style="padding-bottom: 12%; padding-top: 100%;">
+    <h3 class="thin">En este Seminario Aprenderás a:</h3>
         <div class="row">
           <div class="col s4">
             <i class="material-icons" style="font-size: 92px;">attach_money</i>
-            <h5 class="thin">Aqui va un titulo</h5>
-            <p class="thin">loren Ipsum temas de descricpcion aparafrasear lo que de dice el loco</p>
+            <p class="thin">Analizar indicadores  en mercados financieros digitales para rentabilizar</p>
           </div>
           <div class="col s4">
           <i class="material-icons" style="font-size: 92px;">attach_money</i>
-            <h5 class="thin">Aqui va un titulo</h5>
-            <p class="thin">loren Ipsum temas de descricpcion aparafrasear lo que de dice el loco</p>
+            <p class="thin">Operaciones, sistemas y uso de plataformas de trading</p>
           </div>
           <div class="col s4">
             <i class="material-icons" style="font-size: 92px;">attach_money</i>
-            <h5 class="thin">Aqui va un titulo</h5>
-            <p class="thin">loren Ipsum temas de descricpcion aparafrasear lo que de dice el loco</p>
+            <p class="thin">Estrategias, Riesgos y mejores prácticas</p>
           </div>
         </div>
   </div>
